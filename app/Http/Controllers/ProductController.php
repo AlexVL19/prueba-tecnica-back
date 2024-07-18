62abb47954ce440fad676e2c414a2f429c1ce4ab
AlexVL19 <valenciaalexis564@gmail.com>
2024-07-18 12:52:32 -0500

    Controlador de productos terminado

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,14 +7,18 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
+    /* Función que permite mostrar los productos que están activos en la base de datos.
+    TODO: añadir a la query una condición que permite no mostrar los registros que no tengan stock */
     public function showProducts() {
-        $query_show_products = 'SELECT * FROM products WHERE is_active = 1';
+        $query_show_products = 'SELECT * FROM product WHERE is_active = 1';
 
         $result_show_products = DB::connection()->select(DB::raw($query_show_products));
 
         return $result_show_products;
     }
 
+    /* Función que permite añadir un producto; primero valida los datos de la petición, luego ejecuta la query con los datos
+    ya validados. */
     public function addProduct(Request $request) {
 
         $fields = $request->validate([
@@ -24,7 +28,7 @@ class ProductController extends Controller
             'stock_inicial' => 'required',
         ]);
 
-        $query_add_products = 'INSERT INTO products (code, product_name, price, stock) VALUES (?, ?, ?, ?)';
+        $query_add_products = 'INSERT INTO product (code, product_name, price, stock) VALUES (?, ?, ?, ?)';
 
         return DB::connection()->select(DB::raw($query_add_products), [
             $fields['codigo'],
@@ -34,7 +38,41 @@ class ProductController extends Controller
         ]);
     }
 
-    public function updateProduct(Request $request) {
+    /* Función que permite desactivar un producto con base a su ID. Dentro del objeto request se encuentra la ID del producto */
+    public function deleteProduct(Request $request) {
+        $query_delete_products = "UPDATE product SET is_active = 0 WHERE id_product = ?";
         
+        return DB::connection()->select(DB::raw($query_delete_products), [$request->id]);
+    }
+
+    /* Función que permite capturar un producto específico con base a su ID. Se devuelve el registro que se busca. */
+    public function getProduct(Request $request) {
+        $query_get_product = "SELECT * FROM product WHERE id_product = ? LIMIT 1";
+
+        return DB::connection()->select(DB::raw($query_get_product), [$request->id]);
+    }
+
+    /* Función que permite actualizar los datos de un producto específico con base a su ID. Primero se validan los datos y luego
+    se ejecuta la query con los campos ya validados, añadiendo su ID. */
+    public function updateProduct(Request $request) {
+        $fields = $request->validate([
+            'id' => 'required',
+            'codigo' => 'required',
+            'nombre_producto' => 'required|string',
+            'precio' => 'required',
+            'stock_inicial' => 'required',
+        ]);
+
+        $query_update_product = "UPDATE product SET code = ?, product_name = ?, price = ?, stock = ? WHERE id_product = ?";
+
+        return DB::connection()->select(DB::raw($query_update_product), [
+            $fields['codigo'],
+            $fields['nombre_producto'],
+            $fields['precio'],
+            $fields['stock_inicial'],
+            $fields['id'],
+        ]);
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+/* Aquí se registran las rutas de la API del controlador de productos; cada función contiene un link asociado a ello, que se 
+complementa con la dirección del host y el puerto. */
+Route::controller(ProductController::class)->group(function() {
+    Route::get('/showProducts', 'showProducts');
+    Route::post('/addProducts', 'addProduct');
+    Route::put('/deleteProduct', 'deleteProduct');
+    Route::post('/getProduct', 'getProduct');
+    Route::put('/updateProduct', 'updateProduct');
 });
